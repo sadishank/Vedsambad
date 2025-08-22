92549745f6158fd40a840b823bc4a04d26835ff8
Sadishan Khadka <sadishank@gmail.com>
2025-08-22 08:22:57 +0545

    newscomit

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -6,3 +6,15 @@ const RightSidebar = () => {
   const { selectedUser, messages } = useContext(ChatContext);
   const { logout, onlineUsers } = useContext(AuthContext);
   const [msgImages, setMsgImages] = useState([]);
+  useEffect(() => {
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
+
+  return (
+    selectedUser && (
+      <div className="bg-[#8581B2]/10 text-white w-80 h-full relative overflow-y-scroll">
+        <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || "./assets/images/avatarIcon.png"}
+            className="w-20 aspecr-[1/1] rounded-full"
+          />
